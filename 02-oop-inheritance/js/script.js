ce4d3d00f0d4b856daad844d64d96606938c9c19
alvapantanetti <alvaro.pantanetti@gmail.com>
2015-10-03 19:05:21 -0300

    [AlvaroPantanetti]-[Topic 2]-[Re-did the topic entirely]

--- 02-oop-inheritance/js/script.js
+++ 02-oop-inheritance/js/script.js
@@ -3,7 +3,6 @@ var ObserverList = function () {
 };
 
 ObserverList.prototype = {
-
     constructor: ObserverList,
 
     subscribeObserver: function (observer) {
@@ -15,14 +14,13 @@ ObserverList.prototype = {
     }
 };
 
-var Movie = function (title,year,observer) {
+var Movie = function (title,year) {
     this.title = title;
     this.year = year;
-    ObserverList.call(observer);
+    ObserverList.call(this);
 };
 
 Movie.prototype = {
-
     constructor: Movie,
 
     get: function (attribute) {
@@ -34,24 +32,37 @@ Movie.prototype = {
     },
 
     startplaying: function () {
-        for( i = 0; i < observerList.length ; i++) {
-            observerList[i].notifyPlaying(this);
+        var i = 0;
+        for (i; i < this.observerList.length; i+=1) {
+            this.observerList[i].notifyPlaying(this);
         }
     },
 
     stopPlaying: function () {
-        for( i = 0; i < observerList.length; i++) {
-            observerList[i].notifyStop(this);
+        var i = 0;
+        for (i; i < this.observerList.length; i+=1) {
+            this.observerList[i].notifyStop(this);
         }
     }
 };
+
+var extend = function (destination, source) {
+    for (var attribute in source) {
+        if(source.hasOwnProperty(attribute)){
+            destination[attribute] = source[attribute];
+        }
+    }
+    return destination;
+};
+
+extend(Movie.prototype, ObserverList.prototype);
+
 
 var MovieObserver = function (name) {
     this.name = name;
 };
 
 MovieObserver.prototype = {
-
     constructor: MovieObserver,
 
     notifyPlaying: function (Movie) {
@@ -63,20 +74,48 @@ MovieObserver.prototype = {
     }
 };
 
+var DownloadableMovie = function (title,year) {
+    this.title = title;
+    this.year = year;
+    ObserverList.call(this);
+};
+
+DownloadableMovie.prototype = {
+    constructor: DownloadableMovie,
+
+    download: function () {
+        console.log(this.title + ' Is downloading.');
+    }
+};
+
+extend(DownloadableMovie.prototype, Movie.prototype);
+
+var Social = {
+    share: function (MovieObserver) {
+      console.log(MovieObserver.name + ' is sharing ' + this.title + '.' );
+    },
+
+    like: function (MovieObserver){
+        console.log(MovieObserver.name + ' like ' + this.title + '.' );
+    }
+};
+
+extend(Movie.prototype, Social);
+
 
 var ironMan = new Movie('Iron Man',2008);
 var ironManTwo = new Movie('Iron Man II',2010);
-var ironManThree = new Movie('Iron Man III',2013);
+var ironManThree = new DownloadableMovie('Iron Man III',2013);
 var firstobserver = new MovieObserver('John Doe');
 var secondobserver = new MovieObserver('Jane Doe');
 
-ironMan.observerList.subscribeObserver(firstobserver);
-ironMan.observerList.subscribeObserver(secondobserver);
-
-console.log(ironMan.title,ironMan.year);
-console.log(ironManTwo.title,ironManTwo.year);
-console.log(ironManThree.title,ironManThree.year);
-console.log(firstobserver.name);
-console.log(secondobserver.name);
-
+//ironMan.subscribeObserver(firstobserver);
+//ironMan.subscribeObserver(secondobserver);
+//
+//console.log(ironMan.title,ironMan.year);
+//console.log(ironManTwo.title,ironManTwo.year);
+//console.log(ironManThree.title,ironManThree.year);
+//console.log(firstobserver.name);
+//console.log(secondobserver.name);
+//console.log(firstobserver.notifyPlaying(ironMan));
 
