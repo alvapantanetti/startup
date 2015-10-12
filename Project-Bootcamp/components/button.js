e7845fc38be7acc04cf682b8a043a83025a44d32
alvapantanetti <alvaro.pantanetti@gmail.com>
2015-10-12 19:51:03 -0300

    [AlvaroPantanetti]-[Project]-[updating my directories]

---
+++ Project-Bootcamp/components/button.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var _ = require('lodash');
+
+var Button = React.createClass({
+
+    propTypes: {
+        onClick: React.PropTypes.func
+    },
+
+    render: function() {
+        return(
+            <div>
+                <button {...this.getProps()}>{this.props.children}</button>
+            </div>
+        );
+    },
+
+    getProps: function () {
+        return {
+            onClick: this.handleClick,
+            className: this.props.className
+        }
+    },
+
+    handleClick: function (event) {
+        if(this.props.onClick){
+            this.props.onClick(event)
+        }
+    }
+});
+
+module.exports = Button;
